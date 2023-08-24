49b495c8664b3e1808d3b5374964e9d2287ac875
TakaricsNorbert <ntakarics@gmail.com>
2023-08-24 18:18:44 +0200

    login

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Bejelentkezés</title>
     <style>
+        /* CSS for the login form */
         body {
             font-family: Arial, sans-serif;
             background-image: url('images/car_background.jpg');
@@ -21,7 +22,7 @@
             max-width: 400px;
             padding: 30px;
             border-radius: 8px;
-            background-color: rgba(255, 255, 255, 0.8);
+            background-color: rgba(255, 255, 255, 0.8); /* Áttetsző fehér háttér a formon */
         }
 
         h3#log {
@@ -84,71 +85,13 @@
             align-items: center;
             justify-content: center;
         }
-        .switch {
-            position: relative;
-            display: inline-block;
-            width: 60px;
-            height: 34px;
-        }
-        .switch input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-        }
-        .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: #ccc;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
-        .slider:before {
-            position: absolute;
-            content: "";
-            height: 26px;
-            width: 26px;
-            left: 4px;
-            bottom: 4px;
-            background-color: white;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
-        input:checked + .slider {
-            background-color: #f39c12;
-        }
 
-        input:focus + .slider {
-            box-shadow: 0 0 1px #f39c12;
-        }
-
-        input:checked + .slider:before {
-            -webkit-transform: translateX(26px);
-            -ms-transform: translateX(26px);
-            transform: translateX(26px);
-        }
-        .slider.round {
-            border-radius: 34px;
-        }
-
-        .slider.round:before {
-            border-radius: 50%;
-        }
     </style>
 </head>
 <body>
 <div id="login">
     <form id="form" name="login" method="post" action="login_bg.php">
         <h3 id="log">Bejelentkezés</h3>
-        <span style="color: black;">Felhasználó</span>
-        <label class="switch">
-            <input type="checkbox" name="checkbox" value="customer">
-            <span class="slider round"></span>
-        </label>
-        <span style="color: black;">Személyzet</span>
         <label for="username">Felhasználónév:</label>
         <input type="text" id="username" name="username" maxlength="30" size="60" autofocus placeholder="Felhasznalónév">
         <br>
